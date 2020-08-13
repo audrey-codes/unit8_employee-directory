cc4f2d4e8f8b22578635695544fbc270984d683a
Audrey Bazyk <audreybazyk@gmail.com>
2020-08-13 08:44:02 -0500

    Added modal & some style changes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let employees = [];
 const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture, email, location, phone, dob &noinfo &nat=US`;
 const gridContainer = document.querySelector(".grid-container");
 const overlay = document.querySelector(".overlay");
-const modalContent = document.querySelector(".modal-content");
+const modalContainer = document.querySelector(".modal-content");
 const modalClose = document.querySelector(".modal-close");
 const back = document.querySelector(".back");
 const forward = document.querySelector(".forward");
@@ -31,7 +31,7 @@ function displayEmployees(employeeData) {
         let picture = employee.picture;
         employeeHTML += `
         <div class="card" data-index="${index}">
-        <img class="photo" src="${picture.large}" />
+        <img class="avatar" src="${picture.large}" />
         <div class="text-container">
         <h2 class="name">${name.first} ${name.last}</h2>
         <p class="email">${email}</p>
@@ -42,3 +42,63 @@ function displayEmployees(employeeData) {
     });
     gridContainer.innerHTML = employeeHTML;
 }
+
+
+// Display Pop-Up Modal
+
+function displayModal(index) {
+    let { name, dob, phone, email, location: { city, street, state, postcode }, picture } = employees[index];
+    let date = new Date(dob.date);
+    const modalHTML = `
+    <img class="avatar" src="${picture.large}" />
+    <div class="text-container">
+    <h2 class="name">${name.first} ${name.last}</h2>
+    <p class="email">${email}</p>
+    <p class="address">${city}</p>
+    <hr />
+    <p>${phone}</p>
+    <p class="address">${street.number} ${street.name}, ${state} ${postcode}</p>
+    <p>Birthday: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+    </div>
+    `;
+    overlay.classList.remove("hidden");
+    modalContainer.innerHTML = modalHTML;
+}
+
+
+// Open Pop-Up
+
+gridContainer.addEventListener('click', e => {
+    if (e.target !== gridContainer) {
+        const card = e.target.closest(".card");
+        const index = card.getAttribute('data-index');
+        currentIndex = index;
+        displayModal(index);
+    }
+});
+
+
+// Modal Scroll Back
+
+back.addEventListener('click', () => {
+    if (currentIndex > 0) {
+        currentIndex --;
+        displayModal(currentIndex);
+    } else if (currentIndex === 0) {
+        currentIndex = 11;
+        displayModal(currentIndex);
+    }
+});
+
+
+// Modal Scroll Forward
+
+forward.addEventListener('click', () => {
+    if (currentIndex < 11) {
+        currentIndex ++;
+        displayModal(currentIndex);
+    } else if (currentIndex === 11) {
+        currentIndex = 0;
+        displayModal(currentIndex);
+    }
+});
